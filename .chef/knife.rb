11ec0a3060ab16e4587323022b71edcdeb98cca7
xpinjection <lumii.subscriber@gmail.com>
2013-11-20 20:04:05 +0200

    Initial chef files

---
+++ .chef/knife.rb
@@ -0,0 +1,13 @@
+# See http://docs.opscode.com/config_rb_knife.html for more information on knife configuration options
+
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "xpinjection"
+client_key               "#{current_dir}/xpinjection.pem"
+validation_client_name   "xp-injection-validator"
+validation_key           "#{current_dir}/xp-injection-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/xp-injection"
+cache_type               'BasicFile'
+cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
+cookbook_path            ["#{current_dir}/../cookbooks"]
